9494af4069ef029d4a76fe52a5b9e1970dac8514
Battalaxic <79813995+Battalaxic@users.noreply.github.com>
2022-09-10 12:22:13 +1000

    feeling gud rn

--- password_testing.py
+++ password_testing.py
@@ -3,32 +3,73 @@ import os
 import json
 import base64
 
-users = {} # A simple demo storage
+
 
 # Add a user
 username = 'Brent' # The users username
 password = 'mypassword' # The users password
 
-salt = os.urandom(32) # A new salt for this user
-key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-salt = base64.b64encode(salt).decode('ascii')
-key = base64.b64encode(key).decode('ascii')
-users[username] = { # Store the salt and key
-    'salt': salt,
-    'key': key
-}
+users = {} # A simple demo storage
+
+def add_empty_dict_if_empty():
+    empty_flag = False
+    with open("static/user_ids.txt") as f:
+        for line in f:
+            if line == '':
+                empty_flag = True
+    if empty_flag == True:
+        with open("static/user_ids.txt", "w") as f:
+            f.write('{}')
+
+def update_user_ids_file(users):
+    with open("static/user_ids.txt", "w") as outfile:
+        json.dump(users, outfile)
+
+def add_user(username, password):
+    with open("static/user_ids.txt") as f:
+        for line in f:
+            users = json.loads(line)
+    salt = os.urandom(32) # A new salt for this user
+    key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    salt = base64.b64encode(salt).decode('ascii')
+    key = base64.b64encode(key).decode('ascii')
+    users[username] = { # Store the salt and key
+        'salt': salt,
+        'key': key
+    }
+    update_user_ids_file(users)
+    return users
 
 # Verification attempt 1 (incorrect password)
-username = 'Brent'
-password = 'notmypassword'
+# username = 'Brent'
+# password = 'notmypassword'
 
-salt = base64.b64decode(users[username]['salt']) # Get the salt
-key = users[username]['key'] # Get the correct key
-new_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+def login_attempt(username, entered_password):
+    with open("static/user_ids.txt") as f:
+        for line in f:
+            users = json.loads(line)
+    if users.get(username) != None:
+        salt = base64.b64decode(users[username]['salt']) # Get the salt
+        key = users[username]['key'] # Get the correct key
+        new_key = hashlib.pbkdf2_hmac('sha256', entered_password.encode('utf-8'), salt, 100000)
+        new_key = base64.b64encode(new_key).decode('ascii')
+        if key == new_key:
+            return True
+        return False
+    else:
+        return None
 
-print(key == new_key, key, "==", new_key)
+def delete_user(username, password):
+    login_status = login_attempt(username, password)
+    if login_status == True:
+        with open("static/user_ids.txt") as f:
+            for line in f:
+                users = json.loads(line)
+        del users[username]
+        update_user_ids_file(users)
 
-print(users)
 
-with open("static/user_ids.txt", "w") as outfile:
-    json.dump(users, outfile)
+if __name__ == '__main__':
+    add_empty_dict_if_empty()
+    add_user("Test", "blood")
+    print(login_attempt("Test", "blood"))
